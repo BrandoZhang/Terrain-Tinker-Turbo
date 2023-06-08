5000234d9a000296c9f8766005dbc8e9bfe2240c
YuehCorazon <yinhanli@usc.edu>
2023-06-07 20:22:44 -0700

    Add gravity feature on the red vehicle only.

--- Terrain-Tinker-Turbo-Source/Assets/Scripts/Vehicle2.cs
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/Vehicle2.cs
@@ -6,16 +6,11 @@ public class Vehicle2 : MonoBehaviour
 {
     private float speed  = 15.0f;
     private float turnSpeed = 45.0f;
-
-    //public string forwardKey;
-    //public string backwardKey;
-    //public string leftKey;
-    //public string rightKey;
+    
     public Rigidbody rb;
     private float moveVertical;
     private Vector3 newVelocity;
-    Vector3 movement;
-    Vector3 rotationalTorque;
+
 
     private float horizontalInput;
     private float forwardInput;
@@ -29,18 +24,11 @@ public class Vehicle2 : MonoBehaviour
     
     void Update()
     {
-        //horizontalInput = Input.GetAxis("Horizontal");
-        //forwardInput = Input.GetAxis("Vertical");
-        // Move the vehicle forward
-        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        //transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+;
         float moveHorizontal = 0;
-        //float moveVertical = 0;
 
         if (Input.GetKey(KeyCode.I))
         {
-            //Debug.Log("Moving Player");
             moveVertical = 1;
         }
         if (Input.GetKey(KeyCode.K))
@@ -50,28 +38,25 @@ public class Vehicle2 : MonoBehaviour
         if (Input.GetKey(KeyCode.J))
         { 
             transform.Rotate(Vector3.down * turnSpeed * Time.deltaTime);
-            //moveHorizontal = -1; 
         }
         if (Input.GetKey(KeyCode.L))
         { 
             transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
-            //moveHorizontal = 1; 
         }
-
-        movement = new Vector3(0.0f, 0.0f, moveVertical);
-        rotationalTorque = new Vector3(0.0f, moveHorizontal, 0.0f);
-        //movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
 
 
     }
     void FixedUpdate() 
     {
-        //Debug.Log(movement * speed);
-        //rb.AddTorque(rotationalTorque * turnSpeed);
         newVelocity = new Vector3(transform.forward.x, 0.0f, transform.forward.z);
         rb.velocity = newVelocity * moveVertical * speed;
-        //rb.AddForce(transform.forward * moveVertical * speed);
         
+        //check if the car is airborne
+        if (!Physics.Raycast(transform.position, -Vector3.up, 0.5f))
+        {
+            //apply a downward force to the car
+            rb.AddForce(-Vector3.up * 10);
+        }
     }
 }
