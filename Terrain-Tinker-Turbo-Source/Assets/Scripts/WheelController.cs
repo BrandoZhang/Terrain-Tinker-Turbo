e601bb94de1298085ea2cd83b081bdc6d76f84c8
Brando Zhang <zhang@brando.dev>
2023-06-22 22:12:24 -0700

    Feat: Add Reset function for WheelController. Fix bug of center road height.

--- Terrain-Tinker-Turbo-Source/Assets/Scripts/WheelController.cs
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/WheelController.cs
@@ -19,6 +19,9 @@ public class WheelController : MonoBehaviour
     public float acceleration = 3000f;
     public float breakingForce = 30f;
     public float maxTurnAngle = 50f;
+    public Transform startTransform;  // The Transform component where the racer will reset to
+    public float minHeightThreshold = 20f;  // It is considered fall out of the Track if y value is less than this
+    private Rigidbody rb;
 
     private float currentAcceleration = 0f;
     private float currentBreakingForce = 0f;
@@ -26,13 +29,21 @@ public class WheelController : MonoBehaviour
     
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        // Freeze the racer until it can move
+        if (!canMove) return;
+        // Check if the vehicle's height is below a certain threshold
+        if (transform.position.y < minHeightThreshold) 
+        {
+            ResetToStart();
+        }
+        // Get the horizontal and vertical input (up/down and left/right)
+        // Use different axes based on the player index
         float horizontalInput = Input.GetAxis("Horizontal" + (playerIndex == 1 ? "_P1" : "_P2"));
         float verticalInput = Input.GetAxis("Vertical" + (playerIndex == 1 ? "_P1" : "_P2"));
         
@@ -73,7 +84,14 @@ public class WheelController : MonoBehaviour
         UpdateWheel(backRight, backRightTransform);
         
     }
-
+    public void ResetToStart()
+    {
+        Debug.Log("ResetToStart called for player " + playerIndex);
+        rb.velocity = Vector3.zero;  // Reset velocity
+        rb.angularVelocity = Vector3.zero;  // Reset angular velocity
+        transform.position = startTransform.position;  // Reset position
+        transform.rotation = startTransform.rotation;  // Reset rotation
+    }
     void UpdateWheel(WheelCollider col, Transform trans)
     {
         Vector3 pos;
