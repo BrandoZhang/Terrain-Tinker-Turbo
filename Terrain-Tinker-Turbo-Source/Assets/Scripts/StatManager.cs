199591851d3b0353a8fb23045b7ac2a334fc376f
Haowei Liu <92769795+JamesL404@users.noreply.github.com>
2023-06-25 15:56:47 -0700

    Update StatManager.cs

--- Terrain-Tinker-Turbo-Source/Assets/Scripts/StatManager.cs
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/StatManager.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [SerializeField]
 public class StatManager
 {
     public string winner;
+    public string currentTime;
+    public string gameVersion;
+    public string currentScene;
 
-    public StatManager(string winner)
+    public StatManager(string winner, string sceneName)
     {
+        currentTime = System.DateTime.Now.ToString("HH:mm dd MMMM, yyyy");
+        gameVersion = "June 25th";
+        currentScene = sceneName;
         this.winner = winner;
     }
 }
