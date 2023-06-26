b3d62b4ded0b62eb9ee85e61b221a78076f9aaf7
Brando Zhang <zhang@brando.dev>
2023-06-25 22:49:59 -0700

    Feat: Align the behavior of red car with blue car across all scenes.

--- Terrain-Tinker-Turbo-Source/Assets/Scripts/AcceleratorTerrainController.cs
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/AcceleratorTerrainController.cs
@@ -21,7 +21,8 @@ public class AcceleratorTerrainController : MonoBehaviour
             Rigidbody racerRigidbody = other.gameObject.GetComponent<Rigidbody>();
 
             // Apply a force to the racer in the direction of acceleration.
-            if (racerRigidbody.GetComponent<RacerController>() != null)
+            if (racerRigidbody.GetComponent<RacerController>() != null 
+                && racerRigidbody.GetComponent<RacerController>().isActiveAndEnabled)
             {
                 racerRigidbody.AddForce(accelerationDirection * accelerationStrength, ForceMode.Force);
             }
