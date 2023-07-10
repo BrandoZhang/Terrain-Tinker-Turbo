7575464325a87377ca567c1c2f01e1e2322e76eb
YuehCorazon <yinhanli@usc.edu>
2023-07-10 00:14:06 -0700

    Hoovering Tooltip Text. When mouse cursor hoovers on an game object, show text which explains the functionality of certain game object near the cursor.

---
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/TileFunctionality.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileFunctionality : MonoBehaviour
+{
+    public string functionalityDescription;
+
+    public string GetFunctionalityDescription()
+    {
+        return functionalityDescription;
+    }
+}
