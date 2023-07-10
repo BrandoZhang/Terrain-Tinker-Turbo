429ca294ab3e5dfc135ad451f78712c9bf0bb3c5
YuehCorazon <yinhanli@usc.edu>
2023-07-09 23:23:16 -0700

    Fixed the acceleration direction bug aroused by conflicts between rotation of tiles and adding a ramp on the tile.

--- Terrain-Tinker-Turbo-Source/Assets/Scripts/AcceleratorRampController.cs
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/AcceleratorRampController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcceleratorTerrainController : MonoBehaviour
+public class AcceleratorRampController : MonoBehaviour
 {
     // The direction of acceleration. This should be set in the Unity editor.
     public Vector3 accelerationDirection;
@@ -11,17 +11,17 @@ public class AcceleratorTerrainController : MonoBehaviour
     // This function is called when an vehicle enters the terrain's collider.
     private void OnTriggerStay(Collider other)
     {
-        // Debug.Log("Accelerator Triggered");
-        // Check if the other object is a racer (player1 or player2).
-        // Remember to add a "Player1" and "Player2" tag to respective players.
         if (other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))
         {
             Debug.Log("Accelerator: Detect the other part is player.");
             // Get the Rigidbody component of the racer.
             Rigidbody racerRigidbody = other.GetComponentInParent<Rigidbody>();
 
+            // Convert acceleration direction from local space to world space
+            Vector3 worldSpaceAcceleration = transform.TransformDirection(accelerationDirection);
+
             // Apply a force to the racer in the direction of acceleration.
-            racerRigidbody.AddForce(accelerationDirection * accelerationStrength, ForceMode.Force);
+            racerRigidbody.AddForce(worldSpaceAcceleration * accelerationStrength, ForceMode.Force);
         }
     }
 }
