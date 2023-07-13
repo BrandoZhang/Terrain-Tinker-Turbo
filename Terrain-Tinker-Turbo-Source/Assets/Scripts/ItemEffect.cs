dc1524025c294aaad33a94d465a2a10189bdd36a
Brando Zhang <zhang@brando.dev>
2023-07-13 13:01:42 -0700

    Fix: Reverse control not work in WebGL Build.

--- Terrain-Tinker-Turbo-Source/Assets/Scripts/ItemEffect.cs
+++ Terrain-Tinker-Turbo-Source/Assets/Scripts/ItemEffect.cs
@@ -28,16 +28,16 @@ public class ItemEffect : MonoBehaviour
                 if (other.gameObject.CompareTag("Player1"))
                 {
                     // Perform reversing left and right control for player 2
-                    VehicleControl player2Control = GameObject.FindWithTag("Player2").GetComponent<VehicleControl>();
-                    GameManager.Instance.StartCoroutine(player2Control.ReverseControlForSeconds(ReverseLeftRightDuration, 2));
+                    VehicleControl racer2 = GameManager.Instance.racer2;
+                    racer2.StartCoroutine(racer2.ReverseControlForSeconds(ReverseLeftRightDuration, 2));
                     // This is a one-time item
                     gameObject.SetActive(false);
                 }
                 if (other.gameObject.CompareTag("Player2"))
                 {
                     // Perform reversing left and right control for player 1
-                    VehicleControl player1Control = GameObject.FindWithTag("Player1").GetComponent<VehicleControl>();
-                    GameManager.Instance.StartCoroutine(player1Control.ReverseControlForSeconds(ReverseLeftRightDuration, 1));
+                    VehicleControl racer1 = GameManager.Instance.racer1;
+                    racer1.StartCoroutine(racer1.ReverseControlForSeconds(ReverseLeftRightDuration, 1));
                     // This is a one-time item
                     gameObject.SetActive(false);
                 }
